3536abaeec41891329fcad3ad92adf9bf1c6672b
sunfan1 <623451735@qq.com>
2025-08-06 16:26:43 +0800

    upd

--- src/types/video.ts
+++ src/types/video.ts
@@ -351,4 +351,5 @@ export interface ReactVideoProps extends ReactVideoEvents, ViewProps {
   allowsExternalPlayback?: boolean; // iOS
   controlsStyles?: ControlsStyles; // Android
   disableAudioSessionManagement?: boolean; // iOS
+  channel?: number; // Android
 }
